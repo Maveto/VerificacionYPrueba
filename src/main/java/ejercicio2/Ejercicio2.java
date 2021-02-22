8eefaad3f5d4c14e03dc26c29d54ab6f4045f6e3
Maveto <maveto13@gmail.com>
2021-02-22 07:45:56 -0400

    Tarea Ejercicio 2

--- src/main/java/ejercicio2/Ejercicio2.java
+++ src/main/java/ejercicio2/Ejercicio2.java
@@ -2,11 +2,13 @@ package ejercicio2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class Ejercicio2 {
 
     public String getNextDate(int day, String month, int year, int hour, int min, int sec) {
-        String msg;
+        String msg = "";
+        String mon = month.toLowerCase();
         List<String> months = new ArrayList<>();
         months.add("enero");
         months.add("febrero");
@@ -21,12 +23,116 @@ public class Ejercicio2 {
         months.add("noviembre");
         months.add("diciembre");
 
-        System.out.println(months.contains(month));
-        if (day<31 && months.contains(month) && year>0 && hour<24 && min<60 && sec<60) {
+        if (day<32 && day>0 && months.contains(month.toLowerCase()) && year>0 && hour<24 && hour>=0 && min<60 && min>=0 && sec<60 && sec>=0) {
             if (sec == 59) {
-                msg = day + " " + month + " " + year + " " + hour + ":" + (min + 1) + ":" + 0;
+                if (min == 59){
+                    if (hour == 23){
+                        switch (month.toLowerCase()){
+                            case "enero":
+                                if (day == 31){
+                                    msg = 1 + " febrero " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                            case "febrero":
+                                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+                                    if (day > 29){
+                                        msg = "Parametro incorrecto";
+                                    }else if (day == 29){
+                                        msg = 1 + " marzo " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                    }else {
+                                        msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                    }
+                                }else {
+                                    if (day > 28){
+                                        msg = "Parametro incorrecto";
+                                    }else if (day == 28){
+                                        msg = 1 + " marzo " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                    }else {
+                                        msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                    }
+                                }
+                                break;
+                            case "marzo":
+                                if (day == 31){
+                                    msg = 1 + " abril " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                            case "abril":
+                                if (day == 30){
+                                    msg = 1 + " mayo " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                            case "mayo":
+                                if (day == 31){
+                                    msg = 1 + " junio " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                            case "junio":
+                                if (day == 30){
+                                    msg = 1 + " julio " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                            case "julio":
+                                if (day == 31){
+                                    msg = 1 + " agosto " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                            case "agosto":
+                                if (day == 31){
+                                    msg = 1 + " septiembre " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                            case "septiembre":
+                                if (day == 30){
+                                    msg = 1 + " octubre " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                            case "octubre":
+                                if (day == 31){
+                                    msg = 1 + " noviembre " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                            case "noviembre":
+                                if (day == 30){
+                                    msg = 1 + " diciembre " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                            case "diciembre":
+                                if (day == 31){
+                                    msg = 1 + " enero " + (year+1) + " " + 0 + ":" + 0 + ":" + 0;
+                                }else {
+                                    msg = (day+1) + " " + mon + " " + year + " " + 0 + ":" + 0 + ":" + 0;
+                                }
+                                break;
+                        }
+                    } else {
+                        msg = day + " " + mon + " " + year + " " + (hour+1) + ":" + 0 + ":" + 0;
+                    }
+                } else {
+                    msg = day + " " + mon + " " + year + " " + hour + ":" + (min + 1) + ":" + 0;
+                }
             } else {
-                msg = day + " " + month + " " + year + " " + hour + ":" + min + ":" + (sec + 1);
+                msg = day + " " + mon + " " + year + " " + hour + ":" + min + ":" + (sec + 1);
             }
         } else {
             msg = "Parametro incorrecto";
--- src/test/java/ejercicio2/Ejercicio2Test.java
+++ src/test/java/ejercicio2/Ejercicio2Test.java
@@ -33,12 +33,67 @@ public class Ejercicio2Test {
     public static Iterable<Object[]> getData() {
         List<Object[]> objects = new ArrayList<>();
 
-        objects.add(new Object[]{31,"enero",2021,23,59,59,"1 febrero 2021 0:0:0"});
-        objects.add(new Object[]{28,"febrero",2000,23,59,59,"29 febrero 2000 0:0:0"});
-        objects.add(new Object[]{30,"marzo",2021,23,59,59,"31 marzo 2021 0:0:0"});
-        objects.add(new Object[]{30,"abril",2021,23,59,59,"1 mayo 2021 0:0:0"});
-        objects.add(new Object[]{1,"mayo",2021,12,59,59,"1 mayo 2021 13:0:0"});
-        objects.add(new Object[]{24,"junio",2021,13,59,59,"18 junio 2021 8:22:0"});
+        objects.add(new Object[]{1, "Enero", 2020,  23, 59, 59, "2 enero 2020 0:0:0"});
+        objects.add(new Object[]{1, "Febrero", 2020,  23, 59, 59, "2 febrero 2020 0:0:0"});
+        objects.add(new Object[]{1, "Marzo", 2020,  23, 59, 59, "2 marzo 2020 0:0:0"});
+        objects.add(new Object[]{1, "Abril", 2020,  23, 59, 59, "2 abril 2020 0:0:0"});
+        objects.add(new Object[]{1, "Mayo", 2020,  23, 59, 59, "2 mayo 2020 0:0:0"});
+        objects.add(new Object[]{1, "Junio", 2020,  23, 59, 59, "2 junio 2020 0:0:0"});
+        objects.add(new Object[]{1, "Julio", 2020,  23, 59, 59, "2 julio 2020 0:0:0"});
+        objects.add(new Object[]{1, "Agosto", 2020,  23, 59, 59, "2 agosto 2020 0:0:0"});
+        objects.add(new Object[]{1, "Septiembre", 2020,  23, 59, 59, "2 septiembre 2020 0:0:0"});
+        objects.add(new Object[]{1, "Octubre", 2020,  23, 59, 59, "2 octubre 2020 0:0:0"});
+        objects.add(new Object[]{1, "Noviembre", 2020,  23, 59, 59, "2 noviembre 2020 0:0:0"});
+        objects.add(new Object[]{1, "Diciembre", 2020,  23, 59, 59, "2 diciembre 2020 0:0:0"});
+        objects.add(new Object[]{31, "Enero", 2020,  23, 59, 59, "1 febrero 2020 0:0:0"});
+        objects.add(new Object[]{28, "Febrero", 2021,  23, 59, 59, "1 marzo 2021 0:0:0"});
+        objects.add(new Object[]{29, "Febrero", 2020,  23, 59, 59, "1 marzo 2020 0:0:0"});
+        objects.add(new Object[]{31, "Marzo", 2020,  23, 59, 59, "1 abril 2020 0:0:0"});
+        objects.add(new Object[]{30, "Abril", 2020,  23, 59, 59, "1 mayo 2020 0:0:0"});
+        objects.add(new Object[]{31, "Mayo", 2020,  23, 59, 59, "1 junio 2020 0:0:0"});
+        objects.add(new Object[]{30, "Junio", 2020,  23, 59, 59, "1 julio 2020 0:0:0"});
+        objects.add(new Object[]{31, "Julio", 2020,  23, 59, 59, "1 agosto 2020 0:0:0"});
+        objects.add(new Object[]{31, "Agosto", 2020,  23, 59, 59, "1 septiembre 2020 0:0:0"});
+        objects.add(new Object[]{30, "Septiembre", 2020,  23, 59, 59, "1 octubre 2020 0:0:0"});
+        objects.add(new Object[]{31, "Octubre", 2020,  23, 59, 59, "1 noviembre 2020 0:0:0"});
+        objects.add(new Object[]{30, "Noviembre", 2020,  23, 59, 59, "1 diciembre 2020 0:0:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 23, 59, 59, "1 enero 2021 0:0:0"});
+
+        objects.add(new Object[]{20, "Febrero", -1, 23, 59, 59, "Parametro incorrecto"});
+        objects.add(new Object[]{31, "Febrero", 2020, 23, 59, 59, "Parametro incorrecto"});
+        objects.add(new Object[]{31, "Diciembres", 2020, 23, 59, 59, "Parametro incorrecto"});
+
+        objects.add(new Object[]{31, "Diciembre", 2020, -1, 59, 59, "Parametro incorrecto"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 0, 59, 59, "31 diciembre 2020 1:0:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 59, 59, "31 diciembre 2020 2:0:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 11, 59, 59, "31 diciembre 2020 12:0:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 12, 59, 59, "31 diciembre 2020 13:0:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 13, 59, 59, "31 diciembre 2020 14:0:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 22, 59, 59, "31 diciembre 2020 23:0:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 23, 59, 59, "1 enero 2021 0:0:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 24, 59, 59, "Parametro incorrecto"});
+
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, -1, 59, "Parametro incorrecto"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 0, 59, "31 diciembre 2020 1:1:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 1, 59, "31 diciembre 2020 1:2:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 29, 59, "31 diciembre 2020 1:30:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 30, 59, "31 diciembre 2020 1:31:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 31, 59, "31 diciembre 2020 1:32:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 58, 59, "31 diciembre 2020 1:59:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 59, 59, "31 diciembre 2020 2:0:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 60, 59, "Parametro incorrecto"});
+
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 1, -1, "Parametro incorrecto"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 1, 0, "31 diciembre 2020 1:1:1"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 1, 1, "31 diciembre 2020 1:1:2"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 1, 29, "31 diciembre 2020 1:1:30"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 1, 30, "31 diciembre 2020 1:1:31"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 1, 31, "31 diciembre 2020 1:1:32"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 1, 58, "31 diciembre 2020 1:1:59"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 1, 59, "31 diciembre 2020 1:2:0"});
+        objects.add(new Object[]{31, "Diciembre", 2020, 1, 1, 60, "Parametro incorrecto"});
+
+        objects.add(new Object[]{39, "Dragon", -1000, 25, 69, 80, "Parametro incorrecto"});
 
 
         return objects;
